1b72149d98cbb47860d5fe38f2b8c93e20a3075c
José Bayoán Santiago Calderón <jbs3hp@virginia.edu>
2020-02-06 01:26:31 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,11 @@ using Test, Documenter, OSSGH
 
 ENV["POSTGIS_HOST"] = get(ENV, "POSTGIS_HOST", "host.docker.internal")
 ENV["POSTGIS_PORT"] = get(ENV, "POSTGIS_PORT", "5432")
-ENV["GITHUB_TOKEN"] = get(ENV, "GITHUB_TOKEN", "")
+# ENV["GITHUB_TOKEN"] = get(ENV, "GITHUB_TOKEN", "")
 
 DocMeta.setdocmeta!(OSSGH,
                     :DocTestSetup,
-                    :(using OSSGH, DataFrames;
+                    :(using OSSGH, DataFrames, Printf;
                       opt = Opt("Nosferican",
                                 ENV["GITHUB_TOKEN"],
                                 host = ENV["POSTGIS_HOST"],
