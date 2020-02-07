701419f7798cdb7937ade556175ed448ccfe3cd0
José Bayoán Santiago Calderón <jbs3hp@virginia.edu>
2020-02-07 11:18:02 -0500

    Test of docs and tests (#4)
    Added the documentation to the test suite.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,7 @@ using Test, Documenter, OSSGH
 
 ENV["POSTGIS_HOST"] = get(ENV, "POSTGIS_HOST", "host.docker.internal")
 ENV["POSTGIS_PORT"] = get(ENV, "POSTGIS_PORT", "5432")
-# ENV["GITHUB_TOKEN"] = get(ENV, "GITHUB_TOKEN", "")
-
-DocMeta.setdocmeta!(OSSGH,
-                    :DocTestSetup,
-                    :(using OSSGH, DataFrames, Printf;
-                      opt = Opt("Nosferican",
-                                ENV["GITHUB_TOKEN"],
-                                host = ENV["POSTGIS_HOST"],
-                                port = parse(Int, ENV["POSTGIS_PORT"]));),
-                    recursive = true)
+ENV["GITHUB_TOKEN"] = get(ENV, "GITHUB_TOKEN", "")
 
 opt = Opt("Nosferican",
           ENV["GITHUB_TOKEN"],
@@ -21,5 +12,26 @@ opt = Opt("Nosferican",
 execute(opt.conn, "DROP SCHEMA IF EXISTS $(opt.schema) CASCADE;")
 
 @testset "OSSGH" begin
-    doctest(OSSGH)
+    using Documenter, OSSGH
+    using OSSGH
+    using OSSGH: BaseUtils, Licenses
+
+    DocMeta.setdocmeta!(OSSGH,
+                       :DocTestSetup,
+                       :(using OSSGH, DataFrames, Printf;
+                        opt = Opt("Nosferican",
+                                  ENV["GITHUB_TOKEN"],
+                                  host = ENV["POSTGIS_HOST"],
+                                  port = parse(Int, ENV["POSTGIS_PORT"]));),
+                       recursive = true)
+
+    makedocs(sitename = "OSSGH",
+             modules = [OSSGH],
+             pages = [
+                 "Home" => "index.md",
+                 "Manual" => "manual.md",
+                 "API" => "api.md"
+             ],
+             source = joinpath("..", "docs", "src"),
+             build = joinpath("..", "docs", "build"))
 end
