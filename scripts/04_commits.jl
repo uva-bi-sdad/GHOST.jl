81fbbf47e4842e603da74dbe1ee79f52403cde79
José Bayoán Santiago Calderón <naoyabpr@gmail.com>
2021-02-22 18:56:48 -0500

    updated commits script
    Signed-off-by: José Bayoán Santiago Calderón <naoyabpr@gmail.com>

--- scripts/04_commits.jl
+++ scripts/04_commits.jl
@@ -4,16 +4,17 @@ setup()
 setup_parallel()
 @unpack conn, schema = GHOST.PARALLELENABLER
 data = execute(conn,
-               String(read(joinpath(dirname(pathof(GHOST)), "assets", "sql", "branches_min_max.sql"))) |>
+               String(read(joinpath(pkgdir(GHOST), "src", "assets", "sql", "branches_min_max.sql"))) |>
                    (obj -> replace(obj, "schema" => schema)) |>
                    (obj -> replace(obj, "min_lim" => 0)) |>
                    (obj -> replace(obj, "max_lim" => 100)),
                not_null = true) |>
     (obj -> getproperty.(obj, :branch))
 time_start = now()
+
 println(time_start)
 @sync @distributed for idx in 1:8:lastindex(data)
-    query_commits_simple(view(data, idx:min(idx + 7, lastindex(data))), 100)
+    query_commits(view(data, idx:min(idx + 7, lastindex(data))), 100)
     sleep(1)
 end
 time_end = now()
